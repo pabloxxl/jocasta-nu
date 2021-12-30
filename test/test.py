56574ec299332d3597f44f9574bc54db68676125
pabloxxl <pabloxxl@fastmail.fm>
2021-12-30 14:23:24 +0100

    Extended tests

--- test/test.py
+++ test/test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import argparse
 import json
-import pprint
 import requests
 
 import dns.resolver
+
+TC_NUMBER = 0
 
 def resolve(fqdn, nameserver, record_type="A"):
     resolver = dns.resolver.Resolver()
@@ -14,40 +15,70 @@ def resolve(fqdn, nameserver, record_type="A"):
     except dns.resolver.NoNameservers:
         result = None
 
-    print(f"{fqdn} -> {result if result else []}")
+    print(f"\t{fqdn} -> {result if result else []}")
     return result
 
 def reset_rules(rest_ip):
-    print("Resetting rules")
+    print("\tResetting rules")
     response = requests.get(f"http://{rest_ip}:8080/clear")
 
 def add_rule(fqdn, rest_ip, action="BLOCK", record_type="A"):
-    print(f"Adding rule: {action} {fqdn} {record_type}")
+    print(f"\tAdding rule: {action} {fqdn} {record_type}")
     response = requests.get(f"http://{rest_ip}:8080/insert?url={fqdn}&action={action}&type={record_type}")
 
-def stats(rest_ip):
-    print(f"Getting statistics")
+def get_statistics(rest_ip):
+    print(f"\tGetting statistics")
     response = requests.get(f"http://{rest_ip}:8080/stats")
-    return json.loads(response.content)
+    output = json.loads(response.content)
+    print(f"\t{output}")
+    return output
+
+def print_header(description):
+    global TC_NUMBER
+    TC_NUMBER += 1
+    print(f"========== TEST CASE {TC_NUMBER} ({description}) ==========")
 
 parser = argparse.ArgumentParser(description='Jocasta Nu tester')
-parser.add_argument('dns_ip',
-    help='DNS server IP' )
+parser.add_argument('dns_ip', help='DNS server IP' )
 
 args = parser.parse_args()
+print_header("Blocked URL")
 reset_rules(args.dns_ip)
 result = resolve("www.google.pl", args.dns_ip)
 assert result
 
+print_header("One blocked URL and one logget URL")
 add_rule("www.google.pl", args.dns_ip)
 add_rule("www.onet.pl", args.dns_ip, action="LOG")
-result = resolve("www.google.pl", args.dns_ip)
-assert not result
-result = resolve("www.onet.pl", args.dns_ip)
-assert result
+assert not resolve("www.google.pl", args.dns_ip)
+assert resolve("www.onet.pl", args.dns_ip)
 
-pprint.pprint(stats(args.dns_ip))
+print_header("Blocked URL cleared by rest api")
 reset_rules(args.dns_ip)
+assert resolve("www.google.pl", args.dns_ip)
 
-result = resolve("www.google.pl", args.dns_ip)
-assert result
+print_header("Mixing A and AAAA records")
+reset_rules(args.dns_ip)
+add_rule("www.google.pl", args.dns_ip, record_type="AAAA")
+assert resolve("www.google.pl", args.dns_ip)
+assert not resolve("www.google.pl", args.dns_ip, record_type="AAAA")
+add_rule("www.google.pl", args.dns_ip, record_type="A")
+assert not resolve("www.google.pl", args.dns_ip)
+assert not resolve("www.google.pl", args.dns_ip, record_type="AAAA")
+
+print_header("Generation of statistics")
+reset_rules(args.dns_ip)
+add_rule("www.google.pl", args.dns_ip)
+resolve("www.google.pl", args.dns_ip)
+stats = get_statistics(args.dns_ip)
+assert stats
+assert len(stats.get("requests", {})) == 1
+
+print_header("Logging queries")
+reset_rules(args.dns_ip)
+add_rule("www.google.pl", args.dns_ip, action="LOG")
+assert resolve("www.google.pl", args.dns_ip)
+assert resolve("www.google.pl", args.dns_ip)
+stats = get_statistics(args.dns_ip)
+assert stats
+assert len(stats.get("requests", {})) == 2
